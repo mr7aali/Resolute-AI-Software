3b133cf2b85a11f534afbc033d69a7f5f52d2ce1
mr7aali <mr7aali@gmail.com>
2023-02-08 23:06:20 +0600

    done

--- src/Page/ViewStudent/ViewStudent.js
+++ src/Page/ViewStudent/ViewStudent.js
@@ -2,6 +2,7 @@ import { Button, MenuItem, TextField } from '@mui/material';
 import React from 'react';
 import AddIcon from '@mui/icons-material/Add';
 import { useForm } from 'react-hook-form';
+import './ViewStudent.css'
 const ViewStudent = ({handleClose,s}) => {
     const classs = [{ value: 'V', label: 'V', }, { value: 'VI', label: 'VI', }, { value: 'VII', label: 'VII', }, { value: 'VIII', label: 'VIII', }, { value: 'IX', label: 'IX', }, { value: 'X', label: 'X', },];
     const division = [{ value: 'Science', label: 'Science', }, { value: 'Commerce', label: 'Commerce', }, { value: 'Arts', label: 'Arts', },
@@ -30,13 +31,15 @@ const ViewStudent = ({handleClose,s}) => {
     return (
         <div  className='add-student-container'>
             <div className='add-student-header'>
-                <h5> Student</h5>
+                <h5 style={{ 
+                    fontSize:'25px'
+                 }}> Student Details</h5>
                 <h4>2 Feb 2023</h4>
 
             </div>
             <form onSubmit={handleSubmit(handleSummition)}>
                 <div className='input-container'>
-                    <div className='first-input-container'>
+                    <div  className='first-input-container input-disable-cursor'>
                         <TextField defaultValue={s.first_name} disabled {...register("first_name")} id="outlined-basic" label="First Name" variant="outlined" />
                         <TextField disabled defaultValue={s.middle_name} {...register("middle_name")} id="outlined-basic" label="Middle Name" variant="outlined" />
                         <TextField disabled defaultValue={s.last_name} {...register("last_name")} id="outlined-basic" label="Last Name" variant="outlined" />
@@ -68,7 +71,7 @@ const ViewStudent = ({handleClose,s}) => {
                             <TextField disabled defaultValue={s.address2} {...register("address2")} id="outlined-basic" label="Address Line 2" variant="outlined" />
                         </div>
                         <div style={{ marginTop: '15px' }} className='first-input-container'>
-                            <TextField disabled defaultValue={s.Landmark}  {...register("Landmark")} id="outlined-basic" label="Landmark" variant="outlined" />
+                            <TextField  disabled defaultValue={s.Landmark}  {...register("Landmark")} id="outlined-basic" label="Landmark" variant="outlined" />
                             <TextField disabled defaultValue={s.City} {...register("City")} id="outlined-basic" label="City" variant="outlined" />
                             <TextField disabled defaultValue={s.Pincode} {...register("Pincode")} id="outlined-basic" label="Pincode" variant="outlined" />
                             <Button onClick={handleClose} variant='outlined' color='error' sx={{height:'50px'}}  startIcon={<AddIcon />} >Close</Button>
